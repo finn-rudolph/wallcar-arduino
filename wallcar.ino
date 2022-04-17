58e2e14797423657d21a41b825bd354e65c6683c
Finn Rudolph <finn.rudolph@t-online.de>
2022-04-17 11:44:03 +0200

    Implement motor control by PWM

--- wallcar.ino
+++ wallcar.ino
@@ -1,4 +1,5 @@
 #include <ArduinoBLE.h>
+#include <Servo.h>
 
 const char[] serviceUuid = "00-00-00-01-22-76-45-74-8a-df-33-af-0e-cd-f2-0f";
 const char[] propUuid = "00-00-00-01-6e-bc-4a-e9-8c-a1-fe-c7-da-94-d7-d2";
@@ -8,6 +9,9 @@ BLEService service(serviceUuid);
 BLEByteCharacteristic propCharacteristic(propUuid);
 BLEByteCharacteristic driveCharacteristic(driveUuid);
 
+Servo prop, drive;
+int propPin, drivePin;
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     if (!BLE.begin()) {
@@ -15,14 +19,18 @@ void setup() {
         while (1);
     }
 
-    BLE.setLocalName("Nano 33");
-    BLE.setAdvertisedService(service);
     service.addCharacteristic(propCharacteristic);
     service.addCharacteristic(driveCharacteristic);
+    BLE.setLocalName("Nano 33 BLE");
+    BLE.setAdvertisedService(service);
+    BLE.addService(service);
 
     propCharacteristic.writeValue(0);
     driveCharacteristic.writeValue(0);
     BLE.advertise();
+
+    prop.attatch(propPin, 1000, 2000);
+    drive.attatch(drivePin, 1000, 2000);
 }
 
 void loop() {
@@ -34,9 +42,12 @@ void loop() {
                 digitalWrite(LED_BUILTIN, 1);
                 delay(5);
                 digitalWrite(LED_BUILTIN, 0);
+                value = propCharacteristic.value();
+                prop.write(value * 10 + 1000);
             }
             if (driveCharacteristic.written()) {
-
+                value = driveCharacteristic.value();
+                drive.write(value * 10 + 1000);
             }
         }    
     }
