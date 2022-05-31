ad30f89d7df0b7e7757fdf3eb9950ab236ae82e8
Finn Rudolph <finn.rudolph@t-online.de>
2022-05-31 08:26:55 +0200

    Fix prop control, add analog drive control

--- ble/ble.ino
+++ ble/ble.ino
@@ -1,13 +1,11 @@
 #include <ArduinoBLE.h>
 #include <Servo.h>
-#define PROP_PIN 21
-#define DRIVE_PIN 23
 
 BLEService service("00000001-2276-4574-8adf-33af0ecdf20f");
 BLEByteCharacteristic propCharacteristic("00000001-6ebc-4ae9-8ca1-fec7da94d7d2", BLERead | BLEWrite);
 BLEByteCharacteristic driveCharacteristic("00000002-e451-4f97-983f-51cce04e34bf", BLERead | BLEWrite);
 
-Servo prop, drive;
+Servo prop;
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
@@ -15,40 +13,35 @@ void setup() {
         digitalWrite(LED_BUILTIN, 1);
         while (1);
     }
-
-    // maybe set pinmode
-
-    service.addCharacteristic(propCharacteristic);
-    service.addCharacteristic(driveCharacteristic);
+    
     BLE.setLocalName("Nano 33 BLE");
     BLE.setAdvertisedService(service);
+    service.addCharacteristic(propCharacteristic);
+    service.addCharacteristic(driveCharacteristic);
     BLE.addService(service);
 
     propCharacteristic.writeValue(0);
     driveCharacteristic.writeValue(0);
     BLE.advertise();
 
-    prop.attach(PROP_PIN, 1000, 2000);
-    drive.attach(DRIVE_PIN, 1000, 2000);
+    prop.attach(D3, 1000, 2000);
+    prop.writeMicroseconds(1500);
+    delay(7000);
 }
 
 void loop() {
     BLEDevice central = BLE.central();
 
     if (central) {
-        while (central.connected()) {
-            digitalWrite(LED_BUILTIN, HIGH);
+        while (central.connected()) {  
             if (propCharacteristic.written()) {
-                digitalWrite(LED_BUILTIN, LOW);
-                delay(200);
-                unsigned char value = propCharacteristic.value();
-                prop.writeMicroseconds(value * 10 + 1000);
-            }
+                unsigned char pval = propCharacteristic.value();
+                prop.writeMicroseconds(pval * 10 + 1000);
+            }        
             if (driveCharacteristic.written()) {
-                unsigned char value = driveCharacteristic.value();
-                drive.writeMicroseconds(value * 10 + 1000);
+                unsigned char dval = driveCharacteristic.value();
+                analogWrite(A0, dval * 2.55);
             }
         }
-        digitalWrite(LED_BUILTIN, LOW);   
     }
 }
